44b0e8e9c65c593d4c186d93ce30b5ce5795279f
Gabriel Kay <gabrielkay1@gmail.com>
2018-11-02 23:09:00 -0500

    Deleted debug nonsense. Modifed main.cpp

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <cstdlib>
+#include <cmath>
+#include <map>
+#include <set>
+#include <string>
+#include <locale>
+#include <iostream>
+#include <fstream>
+
+typedef std::set<std::string> WordSet;
+typedef std::map<std::string, int> RepeatCounter;
+
+Wordset readDictionary(){
+    //TODO
+    return dictionary;
+}
+
+int main() {
+        WordSet dictionary = readDictionary();
+        std::string resumeFile = "";
+        std::cout << "Please enter the name/path of the file to be processed." << std::endl;
+        std::cin >> resumeFile;
+        Resume resume1(resumeFile);
+        //METHODS PERFORMED ON RESUME BELOW
+        //resume.____;
+
+        return 0;
+}
