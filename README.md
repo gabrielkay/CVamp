a01c8835922ad49ca6f3560ab1d37bf8df62f2c4
Gabriel Kay <gabrielkay1@gmail.com>
2018-11-02 21:52:55 -0500

    Made the basic file

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # CVamp
 Takes a CV in the form of a .txt as an input, and outputs suggestions for how to improve their resume.
+
+Features:
+- Spellcheck
+- Passive-voice checker
+- Confirming email address and phone #s are included
+- Finding overused words
+- Checking for verb repetition
+- 
---
+++ Resume.cpp
@@ -0,0 +1,18 @@
+ /**
+  *   * toLowerCase
+  *     * Converts a string into lowercase and also removes any non-letters from the string
+  *       * pre: none
+  *         * post: returns a string with all letters from first string in original order and all lowercase
+  *           */
+void toLowerCase(std::string &word) {
+    for (size_t i = 0; i < word.length(); i++){
+        if (word.length() > 0) {
+            if (word[i] == '@'){
+                validEmail(word);
+            }
+            char changer = word[i];
+            word[i] = std::tolower(changer);
+        }
+    }
+}
+
