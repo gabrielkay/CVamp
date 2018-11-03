50c5e51be59768ac889c5c9b1e3df48fc426d059
Riyan Kabir <riyankabir@Riyans-MacBook-Pro-2.local>
2018-11-03 13:57:44 -0500

    added cmakelist.txt and debug folder

---
+++ cmake-build-debug/CMakeFiles/CVamp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CVamp.dir/main.cpp.o"
+  "CMakeFiles/CVamp.dir/Resume.cpp.o"
+  "CVamp.pdb"
+  "CVamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CVamp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
